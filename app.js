c78db0b86a06cbcf59bdc7db9ae027de2c5fa5a2
Zelalem Antigegn <zelalem.antigegn12@gmail.com>
2020-06-02 02:18:51 +0300

    Hiting the graphql api

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ const log = console.log;
 const PORT = process.env.PORT;
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    console.log(req.body)
+    next();
+  });
 app.use(isAuth);
 
 app.use('/api',graphqlHttp({
@@ -18,6 +29,8 @@ app.use('/api',graphqlHttp({
     rootValue: graphqlResolvers,
     graphiql: true
 }))
+
+
 mongoose.connect(
     `mongodb://${process.env.MONGO_CLOUD_USER}:${process.env.MONGO_CLOUD_PASSWORD}@cluster0-shard-00-00-00gch.mongodb.net:27017,cluster0-shard-00-01-00gch.mongodb.net:27017,cluster0-shard-00-02-00gch.mongodb.net:27017/${process.env.MONGO_CLOUD_DATABASE}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`, {
      useNewUrlParser: true,
